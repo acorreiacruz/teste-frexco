fed375a1d27b97df22b755987dad5c1451d6b188
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-15 16:22:02 -0300

    Criando um User model customizado.

--- api/models.py
+++ api/models.py
@@ -27,3 +27,26 @@ class CustomUserManager(BaseUserManager):
         user = self.create_user(username, email, password, **other_fields)
         user.save()
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ("-id",)
+        db_table = "users"
+
+    first_name = models.CharField(max_length=150, blank=True, null=False)
+    last_name = models.CharField(max_length=150, blank=True, null=False)
+    username = models.CharField(max_length=150, blank=False, null=False, unique=True)
+    email = models.CharField(max_length=150, blank=False, null=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    birth_date = models.DateField(blank=False, null=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    objects = CustomUserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username","email"]
+
+    def __str__(self):
+        return self.username
