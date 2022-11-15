662050e825435ac233cdb945db7f3506968719a6
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-15 17:09:35 -0300

    Definindo as renderers classes do rest_framework.
    Colocando como possíveis formatos de saída JSON, XLSX e CSV.

--- core/settings/rest_framework.py
+++ core/settings/rest_framework.py
@@ -7,6 +7,11 @@ REST_FRAMEWORK ={
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework_xml.renderers.XMLRenderer",
+        "rest_framwork.renderers.JSONRenderer",
+        "rest_framework_csv.renderers.CSVRenderer",
+    ),
 }
 
 
