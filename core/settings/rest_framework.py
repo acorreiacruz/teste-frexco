24502900ea1b1433530684f7e698129c07fcb3a0
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-15 17:03:49 -0300

    Configurando o SimpleJWT no rest_framework.

--- core/settings/rest_framework.py
+++ core/settings/rest_framework.py
@@ -1,3 +1,20 @@
-from typing import List
+from datetime import timedelta
+import os
 
-REST_FRAMEWORK: List[str] = []
+
+REST_FRAMEWORK ={
+    "DEFAULT_AUTHENCIATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'SIGNING_KEY': os.environ.get("JWT_SECRET_KEY"),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
