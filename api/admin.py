3b1c1d52b4fdb6a2b4add120ffb3463de5ade802
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-15 22:38:58 -0300

    Customizando o admin do User.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+class UserModelAdmin(UserAdmin):
+    fieldsets = (
+        ("Authentication Fields", {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("birth_date","last_login", "date_joined")}),
+    )
+
+admin.site.register(User, UserModelAdmin)
