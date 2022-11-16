3c217e5bdf5233f7343d2c98ca5078fe1aec97fa
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-15 22:35:09 -0300

    Criando a CBV UserModelViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework_csv.renderers import CSVRenderer
+from rest_framework.renderers import JSONRenderer
+from drf_excel.renderers import XLSXRenderer
+from api.serializers import UserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+User = get_user_model()
+
+class UserModelViewSet(ModelViewSet):
+    """ User ModelViewSet for make CRUD operations on API"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated,]
+    renderer_classes = [JSONRenderer, CSVRenderer, XLSXRenderer]
+    http_method_names = ["get", "post", "patch", "delete", "head", "options"]
