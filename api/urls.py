3b247bd1afca8c4c1965a9cfdcdd987c7c3f5a48
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-15 22:35:56 -0300

    Corrigindo o urls do app api
    As urls do router não estavam sendo incluídas na urlpatterns.

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,27 @@
+from . import views
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
+from rest_framework.routers import SimpleRouter
+from .serializers import CustomJWTSerializer
 
 
 app_name = "api"
 
 
+router = SimpleRouter()
+router.register(
+    "users",
+    views.UserModelViewSet,
+    "api-user"
+)
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(serializer_class=CustomJWTSerializer), name='token_obtain_pair'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns += router.urls
