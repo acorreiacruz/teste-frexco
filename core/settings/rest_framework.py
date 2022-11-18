49bb4e81cad3905d4ab7cde70c17ebbfd45f8a7b
acorreiacruz <antoniocruzjunior@outlook.com>
2022-11-18 17:58:15 -0300

    Corrigindo a escrita do RENDERERS_CLASSES na config do rest_framework.
    Corrigindo a escrita da chave DEFAULT_RENDERERS_CLASSES na configuração
    do rest_framework.

--- core/settings/rest_framework.py
+++ core/settings/rest_framework.py
@@ -2,24 +2,23 @@ from datetime import timedelta
 import os
 
 
-REST_FRAMEWORK ={
-    "DEFAULT_AUTHENCIATION_CLASSES": (
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ),
-    "DEFAULT_RENDERER_CLASSES": (
+    ],
+    "DEFAULT_RENDERERS_CLASSES": [
         "rest_framework_xml.renderers.XMLRenderer",
         "rest_framwork.renderers.JSONRenderer",
         "rest_framework_csv.renderers.CSVRenderer",
-    ),
+    ],
 }
 
-
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'SIGNING_KEY': os.environ.get("JWT_SECRET_KEY"),
-    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_TYPES': ('Bearer',)
 }
